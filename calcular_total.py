3c72da62158f4aba49b070305c0240f990b50ad3
julianaBaheza18 <julianaceron603@gmail.com>
2026-09-16 22:33:23 -0500

    Refactor input prompts and add newline at end

--- calcular_total.py
+++ calcular_total.py
@@ -3,9 +3,12 @@
 def calcular_total(precio, cantidad):
     total = precio * cantidad
     return total
-  
+
 precio = float(input("Ingrese el precio del producto: "))
 cantidad = int(input("Ingrese la cantidad de productos: "))
+
 resultado = calcular_total(precio, cantidad)
+
 print("El precio total de la compra es: $", resultado)
+
 input("Presione Enter para salir...")
